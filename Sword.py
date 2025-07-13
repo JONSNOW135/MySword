caff66f5b6d04eb02baa0b73e65ed0207cfbea0c
JONSNOW135 <136754734+JONSNOW135@users.noreply.github.com>
2025-07-13 21:03:37 +0300

    Update Sword.py
    added extra swords.

--- Sword.py
+++ Sword.py
@@ -1,4 +1,14 @@
 print("MY SWORD IS "::::::::l>>>>>>>>>>>>>>>")
 print("MY OTHER SWORD IS "::::::::l11>>>>>>>>>>>>>>>")
 
+
+print("MY OTHER SWORD IS "::::::::l11>>>>>>>>>>>>>>>")
+print("MY OTHER SWORD IS "::::::::l11>>>>>>>>>>>>>>>")
+print("MY OTHER SWORD IS "::::::::l11>>>>>>>>>>>>>>>")
+print("MY OTHER SWORD IS "::::::::l11>>>>>>>>>>>>>>>")
+print("MY OTHER SWORD IS "::::::::l11>>>>>>>>>>>>>>>")
+print("MY OTHER SWORD IS "::::::::l11>>>>>>>>>>>>>>>")
+print("MY OTHER SWORD IS "::::::::l11>>>>>>>>>>>>>>>")
+print("MY OTHER SWORD IS "::::::::l11>>>>>>>>>>>>>>>")
+
 import time
